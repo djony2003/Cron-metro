343a651b22e7526b72759d24f824b47d15fcdf19
DJONY S NUNES <djony2003@gmail.com>
2023-03-21 13:01:18 -0300

    21032023
    Manutenção que atualiza a hora a cada segundo , antes não estava atualizando

--- script.js
+++ script.js
@@ -1,106 +1,107 @@
+_mostra_hora = () => {
+  let data = new Date()
+  let hora = data.getHours()
+  let min = data.getMinutes()
+  let sec = data.getSeconds()
+
+  hora = hora.toString().padStart(2, "0")
+  min = min.toString().padStart(2, "0")
+  sec = sec.toString().padStart(2, "0")
+
+  _hora_topo = document.querySelector("._hora")
+  _hora_topo.innerHTML = `${hora}:${min} hs`
+}
+
+setInterval(_mostra_hora,1000)
+
 const _hr = document.querySelector("._hr")
-      const _min = document.querySelector("._min")
-      const _sec = document.querySelector("._sec")
-      const _dec_sec = document.querySelector("._dec_sec")
-      const _h3 = document.querySelector("._h3")
+const _min = document.querySelector("._min")
+const _sec = document.querySelector("._sec")
+const _dec_sec = document.querySelector("._dec_sec")
+const _h3 = document.querySelector("._h3")
 
-      // Criação das variáveis ********************
+// Criação das variáveis ********************
 
-     
-        let data = new Date()
-        let hora = data.getHours()
-        let min = data.getMinutes()
-        let sec = data.getSeconds()
+let h = 0
+let m = 0
+let s = 0
 
-        hora = hora.toString().padStart(2, "0")
-        min = min.toString().padStart(2, "0")
-        sec = sec.toString().padStart(2, "0")
+// ************************************************* */
 
-        _hora_topo = document.querySelector("._hora")
-        _hora_topo.innerHTML = `${hora}:${min} hs`
-    
+let is_Paused = false
 
-      let h = 0
-      let m = 0
-      let s = 0
+// Criei para não chamar o setInterval() no
+// start quando ele ja estiver rodando , se não faz isso o cronometro
+// fica doido pois o setInterval() é chamado novamente no click
 
-      // ************************************************* */
+//************************************************* */
 
-      let is_Paused = false
+// Seta as variáveis com 0
 
-      // Criei para não chamar o setInterval() no
-      // start quando ele ja estiver rodando , se não faz isso o cronometro
-      // fica doido pois o setInterval() é chamado novamente no click
+_default_val = () => {
+  h = 0
+  m = 0
+  s = 0
+}
 
-      //************************************************* */
+// função para adicionar zero a esquerda dos numeros menores que 10
 
-      // Seta as variáveis com 0
+_zero_a_esq = () => {
+  s = s.toString().padStart(2, "0")
+  m = m.toString().padStart(2, "0")
+  h = h.toString().padStart(2, "0")
+}
 
-      _default_val = () => {
-        h = 0
-        m = 0
-        s = 0
-      }
+// Cronometro Geral
 
-      // função para adicionar zero a esquerda dos numeros menores que 10
+_cronometro = () => {
+  s++
 
-      _zero_a_esq = () => {
-        s = s.toString().padStart(2, "0")
-        m = m.toString().padStart(2, "0")
-        h = h.toString().padStart(2, "0")
-      }
+  if (s === 60) {
+    m++
+    s = 0
+  }
 
-      // Cronometro Geral
+  if (m === 60) {
+    h++
+    m = 0
+  }
+  if (h === 24) {
+    h++
+  }
+  _zero_a_esq()
+  _h3.innerHTML = `${h}:${m}:${s}`
+}
 
-      _cronometro = () => {
-        s++
+// Inicia o cronômetro
 
-        if (s === 60) {
-          m++
-          s = 0
-        }
+_start_Interval = () => {
+  if (!is_Paused) {
+    _interval = setInterval(() => {
+      _cronometro()
+    }, 1000)
+    is_Paused = true
+    let _h3 = document.querySelector("._h3")
+    _h3.style.color = "white"
+  }
+}
 
-        if (m === 60) {
-          h++
-          m = 0
-        }
-        if (h === 24) {
-          h++
-        }
-        _zero_a_esq()
-        _h3.innerHTML = `${h}:${m}:${s}`
-      }
+// Pausa o cronômetro
 
-      // Inicia o cronômetro
+_pause_Interval = () => {
+  clearInterval(_interval)
+  is_Paused = false
+  let _h3 = document.querySelector("._h3")
+  _h3.style.color = "#8c9944"
+}
 
-      _start_Interval = () => {
-        if (!is_Paused) {
-          _interval = setInterval(() => {
-            _cronometro()
-          }, 1000)
-          is_Paused = true
-          let _h3 = document.querySelector("._h3")
-          _h3.style.color = "white"
-        }
-      }
+// Para e zera o cronômetro
 
-      // Pausa o cronômetro
-
-      _pause_Interval = () => {
-        clearInterval(_interval)
-        is_Paused = false
-        let _h3 = document.querySelector("._h3")
-        _h3.style.color = "#8c9944"
-      }
-
-      // Para e zera o cronômetro
-
-      _stop = () => {
-        _default_val()
-        _zero_a_esq()
-        _h3.innerHTML = `${h}:${m}:${s}`
-        clearInterval(_interval)
-        is_Paused = false
-        _h3.style.color = "white"
-       
-      }
+_stop = () => {
+  _default_val()
+  _zero_a_esq()
+  _h3.innerHTML = `${h}:${m}:${s}`
+  clearInterval(_interval)
+  is_Paused = false
+  _h3.style.color = "white"
+}
